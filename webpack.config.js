e39dfb070b728dd50826c115086e8e90bf3e9977
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2018-07-08 14:25:56 -0500

    Resolving #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,12 @@
 const path = require("path");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const devMode = process.env.NODE_ENV !== "production";
 const paths = {
   src: path.join(__dirname, "src"),
   dist: path.join(__dirname, "dist")
 };
-
-let plugins = [
-  new CleanWebpackPlugin(paths.dist),
-  new HtmlWebpackPlugin({
-    template: path.join(paths.src, "html", "app.html"),
-    filename: path.join(paths.dist, "index.html"),
-    inject: true,
-    hash: false,
-    minify: {
-      removeComments: devMode ? false : true,
-      collapseWhitespace: devMode ? false : true,
-      minifyJS: devMode ? false : true,
-      minifyCSS: devMode ? false : true
-    }
-  }),
-  new CopyWebpackPlugin([{
-    from: path.join(paths.src, "images", "*.{jpg,webp}"),
-    to: path.join(paths.dist, "images"),
-    flatten: true
-  }])
-];
 
 module.exports = {
   output: {
@@ -58,8 +36,19 @@ module.exports = {
       }
     }
   },
-  plugins: plugins,
-  stats: {
-    excludeAssets: /\.(jpe?g|webp|html?)$/i
-  }
+  plugins: [
+    new CleanWebpackPlugin(paths.dist),
+    new HtmlWebpackPlugin({
+      template: path.join(paths.src, "html", "app.html"),
+      filename: path.join(paths.dist, "index.html"),
+      inject: true,
+      hash: false,
+      minify: {
+        removeComments: devMode ? false : true,
+        collapseWhitespace: devMode ? false : true,
+        minifyJS: devMode ? false : true,
+        minifyCSS: devMode ? false : true
+      }
+    })
+  ]
 };
